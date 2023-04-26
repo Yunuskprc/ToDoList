033aa0f8442de5121587f7bd68ad38adeaadc54e
Tunahan DURAN <tunahanduran1903bjk@gmail.com>
2023-04-26 22:41:33 +0300

    Giriş Ekran kodlaması

---
+++ ToDoListApp/ToDoListApp/GirisEkran.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ToDoListApp
+{
+    partial class GirisEkran
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGiris = new Button();
+            SuspendLayout();
+            // 
+            // btnGiris
+            // 
+            btnGiris.Location = new Point(237, 134);
+            btnGiris.Name = "btnGiris";
+            btnGiris.Size = new Size(75, 23);
+            btnGiris.TabIndex = 0;
+            btnGiris.Text = "Giris";
+            btnGiris.UseVisualStyleBackColor = true;
+            btnGiris.Click += btnGiris_Click;
+            // 
+            // GirisEkran
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnGiris);
+            Name = "GirisEkran";
+            Text = "GirisEkran";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnGiris;
+    }
+}
